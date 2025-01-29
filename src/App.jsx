99787da2596bcc8a807c01d3fe5dbb4341225e50
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-29 01:47:15 -0300

    Começo do projeto.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons'
+
+const API = "http://localhost:5000"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default App;
